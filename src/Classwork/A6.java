c9a1048eeee871e0519a25ee85a5eb1287d59b46
jowahl97 <johannes.wahl@brz.gv.at>
2019-11-21 20:20:49 +0100

    A8 not finished

--- src/Classwork/A6.java
+++ src/Classwork/A6.java
@@ -6,8 +6,8 @@ public class A6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter how many numbers there should be in the array: ");
-        int numOfNumbers = sc.nextInt();
-        int[] intArray = new int[numOfNumbers];
+        int countOfNumbers = sc.nextInt();
+        int[] intArray = new int[countOfNumbers];
         for (int i = 0; i < intArray.length; i++) {
             System.out.print("Enter a number for number " + (i+1) + " : ");
             intArray[i] = sc.nextInt();
--- src/Classwork/A8.java
+++ src/Classwork/A8.java
@@ -2,10 +2,35 @@ package Classwork;
 
 public class A8 {
     public static void main(String[] args) {
-        String[] foodPortion = {"1dl milk", "1 slice of bread", "100 gr chicken breast", "200gr salad"};
+        String[] foodPortion = {"1 dl milk", "slice of bread", "100 gr chicken breast", "200gr salad"};
         int[] kcalPerPortion = {62, 74, 165, 33};
 
-        String[] breakfast = {};
+        String[] breakfast = {"2 x " + foodPortion[0], "3 x " + foodPortion[1], "0 x " + foodPortion[2], "0 x " + foodPortion[3]};
+        String[] lunch = {"0 x " + foodPortion[0], "1 x " + foodPortion[1], "2 x " + foodPortion[2], "1 x " + foodPortion[3]};
+        String[] dinner = {"2 x " + foodPortion[0], "3 x " + foodPortion[1], "0 x " + foodPortion[2], "0 x " + foodPortion[3]};
 
+        System.out.println("BREAKFAST");
+        System.out.println("===========");
+        PortionPerMeal(breakfast);
+        System.out.println("LUNCH");
+        System.out.println("===========");
+        PortionPerMeal(lunch);
+        System.out.println("DINNER");
+        System.out.println("===========");
+        PortionPerMeal(dinner);
+
+    }
+
+    private static void PortionPerMeal(String[] meal) {
+        for (String food : meal) {
+            if (food.contains("0 x ")){
+            } else {
+                System.out.println(food);
+            }
+        }
+    }
+
+    private static void KcalPerMeal(String[] meal, int[] kcal) {
+        
     }
 }
